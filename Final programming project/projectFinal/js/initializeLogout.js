317a5d1f0bc2f080f81b3cc57e1d6a9bfb35b463
Santiago gualotuña <sanexitoxx@gmail.com>
2024-07-22 15:52:29 -0500

    Update initializeLogout.js

--- Final programming project/projectFinal/js/initializeLogout.js
+++ Final programming project/projectFinal/js/initializeLogout.js
@@ -2,7 +2,7 @@ function initializeLogout() {
     document.getElementById('logoutButton').addEventListener('click', function() {
         document.getElementById('ticketBackground').style.display = 'none';
         document.getElementById('loginBackground').style.display = 'flex';
-        document.getElementById('downloadPdfButton').style.display = 'none'; // Ocultar el botón de descarga PDF
+        document.getElementById('downloadPdfButton').style.display = 'none'; 
     });
 }
 
